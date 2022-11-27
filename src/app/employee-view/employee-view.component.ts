fe0b1bdbfa602e8c6e9dfda7e9903560e4fc43d8
abhiraj2912 <abhirajaravind408@gmail.com>
2022-11-27 23:50:41 +0530

    data

--- src/app/employee-view/employee-view.component.ts
+++ src/app/employee-view/employee-view.component.ts
@@ -6,5 +6,37 @@ import { Component } from '@angular/core';
   styleUrls: ['./employee-view.component.css']
 })
 export class EmployeeViewComponent {
+  data =[
+    {
+      "name":"abhiraj",
+      "email":"abhiraj@gmail.com",
+      "phone":"98562147856",
+      "salary":25000
+    },
+    {
+      "name":"alex",
+      "email":"alex@gmail.com",
+      "phone":"98562147856",
+      "salary":26000
+    },
+    {
+      "name":"mike",
+      "email":"mike@gmail.com",
+      "phone":"9856217856",
+      "salary":30000
+    },
+    {
+      "name":"Anil",
+      "email":"anil@gmail.com",
+      "phone":"98562147856",
+      "salary":27000
+    },
+    {
+      "name":"david",
+      "email":"david@gmail.com",
+      "phone":"98562147856",
+      "salary":26000
+    }
+  ]
 
 }
