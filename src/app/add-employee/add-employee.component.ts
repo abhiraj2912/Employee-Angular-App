469b22ee7366ff8a3f08441c4b44abb14236b3d1
abhiraj2912 <abhirajaravind408@gmail.com>
2022-11-28 12:40:00 +0530

    read data

--- src/app/add-employee/add-employee.component.ts
+++ src/app/add-employee/add-employee.component.ts
@@ -6,5 +6,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./add-employee.component.css']
 })
 export class AddEmployeeComponent {
+  name=""
+  email=""
+  phone=""
+  salary=""
+  designation=""
+
+  readValue=()=>
+  {
+    let data={
+      "name":this.name,
+      "designation":this.designation,
+      "email":this.email,
+      "phone":this.phone,
+      "salary":this.salary
+    }
+
+    console.log(data)
+
+  }
 
 }
--- src/app/employee-view/employee-view.component.ts
+++ src/app/employee-view/employee-view.component.ts
@@ -9,30 +9,35 @@ export class EmployeeViewComponent {
   data =[
     {
       "name":"abhiraj",
+      "designation":"Software Engineer",
       "email":"abhiraj@gmail.com",
       "phone":"98562147856",
       "salary":25000
     },
     {
       "name":"alex",
+      "designation":"Software Engineer",
       "email":"alex@gmail.com",
       "phone":"98562147856",
       "salary":26000
     },
     {
       "name":"mike",
+      "designation":"Software Engineer",
       "email":"mike@gmail.com",
       "phone":"9856217856",
       "salary":30000
     },
     {
       "name":"Anil",
+      "designation":"Software Engineer",
       "email":"anil@gmail.com",
       "phone":"98562147856",
       "salary":27000
     },
     {
       "name":"david",
+      "designation":"Software Engineer",
       "email":"david@gmail.com",
       "phone":"98562147856",
       "salary":26000
